fda95f65de6889d196e8dae1c5242011bc5f56d9
Mmx <1624045573@qq.com>
2021-03-16 23:01:38 +0800

    fix:删除遗留Debug信息

--- Util/checker.go
+++ Util/checker.go
@@ -1,7 +1,6 @@
 package Util
 
 import (
-	"fmt"
 	"net"
 )
 
@@ -11,7 +10,6 @@ var Checker checker
 
 func (checker) NetOk() bool {
 	if ip, err := net.LookupIP("www.msftconnecttest.com"); err != nil {
-		fmt.Println("a")
 		return false
 	} else if len(ip) == 0 || ip[0].String() != "13.107.4.52" {
 		return false
