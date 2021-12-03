7490e8c3858b5666577a59d422fe3ae68f47ee23
Mmx <1624045573@qq.com>
2021-12-03 20:10:30 +0800

    fix: 修正main函数逻辑

--- main.go
+++ main.go
@@ -16,30 +16,29 @@ func main() {
 	} else if global.Config.Settings.Guardian.Enable {
 		//进入守护模式流程
 		controllers.EnterGuardian()
-	}
-
-	//单次登录模式
-	if global.Config.Settings.Interfaces == "" { //单网卡
-		if err := controllers.Login(true, false, nil); err != nil {
-			util.Log.Println("运行出错，状态异常")
-			if global.Config.Settings.DemoMode {
-				util.Log.Fatalln(err)
-			} else {
-				util.Log.Println(err)
+	} else {
+		//单次登录模式
+		if global.Config.Settings.Interfaces == "" { //单网卡
+			if err := controllers.Login(true, false, nil); err != nil {
+				util.Log.Println("运行出错，状态异常")
+				if global.Config.Settings.DemoMode {
+					util.Log.Fatalln(err)
+				} else {
+					util.Log.Println(err)
+					return
+				}
+			}
+		} else { //多网卡
+			interfaces, e := util.GetInterfaceAddr()
+			if e != nil {
 				return
 			}
-		}
-	} else { //多网卡
-		interfaces, e := util.GetInterfaceAddr()
-		if e != nil {
-			return
-		}
-		for _, eth := range interfaces {
-			if err := controllers.Login(true, false, eth.Addr); err != nil {
-				util.Log.Println(eth.Name + "运行出错，状态异常")
-				util.Log.Println(err)
+			for _, eth := range interfaces {
+				if err := controllers.Login(true, false, eth.Addr); err != nil {
+					util.Log.Println(eth.Name + "运行出错，状态异常")
+					util.Log.Println(err)
+				}
 			}
 		}
 	}
-
 }
