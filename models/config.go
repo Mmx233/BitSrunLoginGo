21440ca22aa514cd33d4a7fa031d8ee38bd5d49a
Mmx233 <admin@mmxblog.com>
2022-03-02 19:39:22 +0800

    fix: 部分配置无法被正确解析的问题

--- models/config.go
+++ models/config.go
@@ -3,37 +3,37 @@ package srunModels
 import "github.com/Mmx233/BitSrunLoginGo/v1/transfer"
 
 type Daemon struct {
-	Enable bool   `json:"enable"`
-	Path   string `json:"path"`
+	Enable bool   `json:"enable" yaml:"enable" mapstructure:"enable"`
+	Path   string `json:"path" yaml:"path" mapstructure:"path"`
 }
 
 type Guardian struct {
-	Enable   bool `json:"enable"`
-	Duration uint `json:"duration"`
+	Enable   bool `json:"enable" yaml:"enable" mapstructure:"enable"`
+	Duration uint `json:"duration" yaml:"duration" mapstructure:"duration"`
 }
 
 type Basic struct {
-	Https        bool   `json:"https"`
-	Timeout      uint   `json:"timeout"`
-	Interfaces   string `json:"interfaces"`
-	SkipNetCheck bool   `json:"skip_net_check" yaml:"skip_net_check"`
+	Https        bool   `json:"https" yaml:"https" mapstructure:"https"`
+	Timeout      uint   `json:"timeout" yaml:"timeout" mapstructure:"timeout"`
+	Interfaces   string `json:"interfaces" yaml:"interfaces" mapstructure:"interfaces"`
+	SkipNetCheck bool   `json:"skip_net_check" yaml:"skip_net_check" mapstructure:"skip_net_check"`
 }
 
 type Settings struct {
-	Basic    Basic    `json:"basic"`
-	Guardian Guardian `json:"guardian"`
-	Daemon   Daemon   `json:"daemon"`
-	Debug    Debug
+	Basic    Basic    `json:"basic" yaml:"basic" mapstructure:"basic"`
+	Guardian Guardian `json:"guardian" yaml:"guardian" mapstructure:"guardian"`
+	Daemon   Daemon   `json:"daemon" yaml:"daemon" mapstructure:"daemon"`
+	Debug    Debug    `json:"debug" yaml:"debug" mapstructure:"debug"`
 }
 
 type Debug struct {
-	Enable   bool   `json:"enable"`
-	WriteLog bool   `json:"write_log" yaml:"write_log"`
-	LogPath  string `json:"log_path" yaml:"log_path"`
+	Enable   bool   `json:"enable" yaml:"enable" mapstructure:"enable"`
+	WriteLog bool   `json:"write_log" yaml:"write_log" mapstructure:"write_log"`
+	LogPath  string `json:"log_path" yaml:"log_path" mapstructure:"log_path"`
 }
 
 type Config struct {
-	Form     srunTransfer.LoginForm `json:"form"`
-	Meta     srunTransfer.LoginMeta `json:"meta"`
-	Settings Settings               `json:"settings"`
+	Form     srunTransfer.LoginForm `json:"form" yaml:"form" mapstructure:"form"`
+	Meta     srunTransfer.LoginMeta `json:"meta" yaml:"meta" mapstructure:"meta"`
+	Settings Settings               `json:"settings" yaml:"settings" mapstructure:"settings"`
 }
