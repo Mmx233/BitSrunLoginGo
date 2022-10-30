a7a2ee173e27fabb84db509c86af86ed8abdb61c
Mmx233 <admin@mmxblog.com>
2022-10-30 14:13:53 +0800

    chore: 简化 http tool 生成

--- util/http.go
+++ util/http.go
@@ -12,13 +12,7 @@ var httpTools map[net.Addr]*tool.Http
 
 func init() {
 	if global.Config.Settings.Basic.Interfaces == "" {
-		HttpTool = tool.NewHttpTool(tool.GenHttpClient(&tool.HttpClientOptions{
-			Transport: tool.GenHttpTransport(&tool.HttpTransportOptions{
-				Timeout:           global.Timeout,
-				SkipSslCertVerify: global.Config.Settings.Basic.SkipCertVerify,
-			}),
-			Timeout: global.Timeout,
-		}))
+		HttpTool = genHttpTool(nil)
 	} else {
 		httpTools = make(map[net.Addr]*tool.Http, 0)
 	}
@@ -31,15 +25,18 @@ func HttpTools(addr net.Addr) *tool.Http {
 	if addrHttp, ok := httpTools[addr]; ok {
 		return addrHttp
 	} else {
-		addrHttp = tool.NewHttpTool(tool.GenHttpClient(&tool.HttpClientOptions{
-			Transport: tool.GenHttpTransport(&tool.HttpTransportOptions{
-				Timeout:           global.Timeout,
-				LocalAddr:         addr,
-				SkipSslCertVerify: global.Config.Settings.Basic.SkipCertVerify,
-			}),
-			Timeout: global.Timeout,
-		}))
-		httpTools[addr] = addrHttp
+		httpTools[addr] = genHttpTool(addr)
 		return addrHttp
 	}
+}
+
+func genHttpTool(addr net.Addr) *tool.Http {
+	return tool.NewHttpTool(tool.GenHttpClient(&tool.HttpClientOptions{
+		Transport: tool.GenHttpTransport(&tool.HttpTransportOptions{
+			Timeout:           global.Timeout,
+			LocalAddr:         addr,
+			SkipSslCertVerify: global.Config.Settings.Basic.SkipCertVerify,
+		}),
+		Timeout: global.Timeout,
+	}))
 }
