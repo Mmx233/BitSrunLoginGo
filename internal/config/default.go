fb2aaff769fc4b413db03904d4396baeaa510bee
Mmx233 <admin@mmxblog.com>
2023-12-03 20:38:51 +0800

    improve: 添加 windows chrome User-Agent 作为默认值

--- internal/config/default.go
+++ internal/config/default.go
@@ -39,5 +39,8 @@ var defaultConfig = ConfFromFile{
 				"token": "",
 			},
 		},
+		CustomHeader: map[string]interface{}{
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+		},
 	},
 }
