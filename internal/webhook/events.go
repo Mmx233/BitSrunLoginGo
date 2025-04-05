9740ac9a610d6e6918a67e81252fbad58f9e93c3
Mmx <36563672+Mmx233@users.noreply.github.com>
2025-04-05 19:28:42 +0800

    feat: webhook event factories

--- internal/webhook/events.go
+++ internal/webhook/events.go
@@ -1,5 +1,10 @@
 package webhook
 
+import (
+	"sync/atomic"
+	"time"
+)
+
 type EventName string
 
 // Action Events
@@ -27,11 +32,21 @@ type Event interface {
 
 type BaseEvent struct {
 	ID        uint      `json:"id"`
-	Timestamp int64     `json:"timestamp"` // unix
+	Timestamp int64     `json:"timestamp"` // unix milli
 	Name      EventName `json:"name"`
 }
 
 func (BaseEvent) implementWebhookEvent() {}
+
+var EventID atomic.Uint64
+
+func NewBaseEvent(name EventName) BaseEvent {
+	return BaseEvent{
+		ID:        uint(EventID.Add(1)),
+		Timestamp: time.Now().UnixMilli(),
+		Name:      name,
+	}
+}
 
 type ActionEventStatus string
 
@@ -39,6 +54,8 @@ const (
 	ActionEventStatusSuccess ActionEventStatus = "success"
 	ActionEventStatusFailure ActionEventStatus = "failure"
 )
+
+type ActionName string
 
 type ActionEvent struct {
 	BaseEvent
@@ -50,7 +67,32 @@ type ActionEvent struct {
 	ErrorMessage string `json:"error_message,omitempty"`
 }
 
+func NewActionSuccessEvent(eventName EventName, actionName, value string) ActionEvent {
+	return ActionEvent{
+		BaseEvent:  NewBaseEvent(eventName),
+		Status:     ActionEventStatusSuccess,
+		ActionName: actionName,
+		Value:      value,
+	}
+}
+
+func NewActionFailureEvent(eventName EventName, actionName, errMsg string) ActionEvent {
+	return ActionEvent{
+		BaseEvent:    NewBaseEvent(eventName),
+		Status:       ActionEventStatusFailure,
+		ActionName:   actionName,
+		ErrorMessage: errMsg,
+	}
+}
+
 type DataEvent struct {
 	BaseEvent
 	Property interface{} `json:"property"`
+}
+
+func NewDataEvent(eventName EventName, property any) DataEvent {
+	return DataEvent{
+		BaseEvent: NewBaseEvent(eventName),
+		Property:  property,
+	}
 }
